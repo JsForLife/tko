f879f33f66995d0760574c0fd90a6efa7df30a12
Rong <1067536173@qq.com>
2019-07-23 19:52:23 +0800

    change router, make all router auto import

--- src/app.ts
+++ src/app.ts
@@ -5,19 +5,5 @@ const app = new Koa();
 
 app.use(router.middleware());
 
-// app.use(async (ctx, netx) => {
-//   const { request } = ctx;
-//   const { url } = request;
-//   if (url === '/hellow') {
-//     ctx.body = 'Hello';
-//   } else {
-//     await netx();
-//   }
-// });
-
-// app.use(async (ctx) => {
-//   ctx.body = 'Hello World!';
-// });
-
 app.listen(3000);
 console.log('app run');
--- src/router.ts
+++ src/router.ts
@@ -1,19 +1,33 @@
-import login_router from './business/login/router';
 import JoiRouter = require('koa-joi-router');
 import * as _ from 'lodash';
+import * as fs from 'fs';
 
-interface RouterBuilder {
-  (...router:JoiRouter.RouterInstance[]) : JoiRouter.RouterInstance;
+interface GetAllRouters {
+  () : JoiRouter.RouterInstance[];
 }
 
-const router_builder:RouterBuilder = (...routers) => {
+interface RouterBuilder {
+  () : JoiRouter.RouterInstance;
+}
+
+// 约束性的路由, business文件夹下每个业务文件夹均有个router文件
+const get_all_routers:GetAllRouters = () => {
+  const folders = fs.readdirSync(`${__dirname}/business`);
+  return _.map(folders, (folder) => {
+    const router = require(`${__dirname}/business/${folder}/router`);
+    return router.default;
+  });
+};
+
+const router_builder:RouterBuilder = () => {
   const router_collection = JoiRouter();
+  const routers = get_all_routers();
   router_collection.route(_.map(routers, (router) => {
     return router.routes[0];
   }));
   return router_collection;
 };
 
-const api_router = router_builder(login_router);
+const api_router = router_builder();
 
 export default api_router;
