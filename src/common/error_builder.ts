f13e7bca8da7f8725cc3e7b537bd9c91cb564a08
Rong <1067536173@qq.com>
2019-08-14 19:08:12 +0800

    Add error builder

---
+++ src/common/error_builder.ts
@@ -0,0 +1,11 @@
+export class ErrorBuild {
+  constructor() {
+
+  }
+
+  public http_code:number;
+  public error_cord:number;
+  public error_id:number;
+  public message:string;
+  public stack:string;
+}
